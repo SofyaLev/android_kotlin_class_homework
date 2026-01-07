4788003fe7c3022c9b4e8537b85f2fa1894843cc
SofyaLev <sofya_levchuk@bk.ru>
2026-01-07 12:38:22 +0300

    added class DevOpsEngineer

--- app/src/main/java/com/example/kotlinclasshomework/kotlinClass/Employee.kt
+++ app/src/main/java/com/example/kotlinclasshomework/kotlinClass/Employee.kt
@@ -40,3 +40,13 @@ class Teamlead : Employee() {
 }
 
 // TODO здесь можно создать новый класс, который будет наследоваться от класса Employee. Не забудьте переопределить методы haveLunch() и goHome()
+
+class DevOpsEngineer : Employee() {
+    override fun haveLunch() {
+        println("Закажу обед доставкой в офис и продолжу деплоить")
+    }
+
+    override fun goHome() {
+        println("Сегодня ночую в офисе...")
+    }
+}
